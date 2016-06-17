143c8217c5a73451e19a6ce4ce7d6150a2b4ba12
Rohit Ukirde <ukirderohit@gmail.com>
2016-06-17 16:52:55 -0500

    Done Validation , if the username and password is present in csv file then only the user can start the game

--- loginwork.py
+++ loginwork.py
@@ -10,11 +10,15 @@ def database():
     secdata = ['12345']
     d = []
 
-def menu():  
-    print(" ****  Welcome to Login Portal **** ")
+def menu(): 
+    print("")
+    print("")
+    print("")
+    print(" ****  Welcome to Hangman **** ")
     print("")  
-    print("1.New Client")
-    print("2.Existing Client")
+    print("")
+    print("1.New - SignUp Here")
+    print("2.Login")
     print("3.Exit")
     print("")
 	
@@ -48,18 +52,58 @@ def user():
             match = re.search(repasswd,passwd)
             if match:
                 print("         !!! Password Matched !!!      ")
-                with open("Book1.csv", "ab") as csvfile:
-					writerds = csv.writer(csvfile)
-					writerds.writerow([firstname,lastname,emailid,phoneno,username,passwd])
-					csvfile.close()
-					menu()# Game should start here
-					return False
+                conf = int(raw_input(" For confirmation press 1 :  "))
+                if conf == 1:
+                    print("")
+                    print("")
+                    print("     !!!!!!!!!       Regristration successful    !!!!!!!")
+                    print("")
+                
+                    with open("hangman.csv", "ab") as csvfile:
+                        writerds = csv.writer(csvfile)
+                        writerds.writerow([firstname,lastname,emailid,phoneno,username,passwd])
+                        csvfile.close()
+                        menu()# Game should start here
+                        return False
+                else:
+                    menu()
+                    return False
 					
             else:
                 print(" @@@@ password did not matched @@@@ ")
                 os.system('cls')
                 continue
             print("")
+    elif choice == 2:
+        os.system('cls')
+        print("")
+        print("")
+        print(" Welcome to Hangman !! ")
+        print("")
+        while True:
+            username1 = raw_input("Enter your Username : ")
+            passwd1 = raw_input("Enter your password : ")
+            FileOpen = open("hangman.csv","rb")
+            FileRead = csv.reader(FileOpen, delimiter = ',')
+            FileRead.next() #skip first line
+            text=[]
+            text1=[]
+            for row in FileRead:
+                text = row[4]
+                text1 = row[5]
+                if (text in username1) and (text1 in passwd1) :
+                    print "Success"
+                    
+                    menu()
+                    user()
+                    return False
+                else:
+                    i=0
+            if i==0:
+                i=1
+                print "Incorrect username and Password"
+                
+                continue
 database()
 menu()
 user()
